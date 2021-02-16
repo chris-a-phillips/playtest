102cc23a3ddf675182c5410cc2a804037522f7d9
Chris Phillips <phillipsachris@gmail.com>
2021-02-15 18:36:04 -0800

    Restructured folders

---
+++ vanilla-js-playground/interview-questions/fibonnaci.js
@@ -0,0 +1,15 @@
+// Write a function `fib(n)` that takes in a number as an argument
+// The function should return the n-th number of the Fibonacci sequence
+
+// The 1st and 2nd number of the sequence is 1
+// To generate the next number of the sequence, we sum the previous two numbers
+
+// Memoization is when we store values from a prior operation, so that newer iterations don't need to compute the same data
+
+const fib = (n, memo = {}) => {
+	if (n in memo) return memo[n];
+	if (n <= 2) return 1;
+
+	memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+	return memo[n];
+};
