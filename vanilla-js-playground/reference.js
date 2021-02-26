b298f7de4d24c169d0d44815057264dd076d8547
atlfinesse <finesse.cap@gmail.com>
2021-02-26 12:39:55 -0800

    Added getAt method

--- vanilla-js-playground/reference.js
+++ vanilla-js-playground/reference.js
@@ -1,82 +1,84 @@
 class Node {
 	constructor(data) {
 		// a Node starts with a given data property
-        this.data = data
+		this.data = data;
 		// a Node also has a .next property initialized as null
-        this.next = null
+		this.next = null;
 	}
 }
 
 class LinkedList {
 	constructor(head = null) {
 		// a Linked List starts with a "head" property initialized as null
-        this.head = head
+		this.head = head;
 	}
 	appendNode(data) {
 		// creates a new node with the given data and adds it to back of the list
-        let node = new Node(data)
+		let node = new Node(data);
 
-        if(!this.head) {
-            this.head = node
-            return this.head
-        }
+		if (!this.head) {
+			this.head = node;
+			return this.head;
+		}
 
-        let tail = this.head
-        while(tail.next !== null) {
-            tail = tail.next
-        }
-        tail.next = node
-        return this.head
+		let tail = this.head;
+		while (tail.next !== null) {
+			tail = tail.next;
+		}
+		tail.next = node;
+		return this.head;
 	}
 	prependNode(data) {
 		// creates a new node with the given data and adds it to the front of the list
-        let node = new Node(data)
+		let node = new Node(data);
 
-        node.next = this.head;
-        this.head = node
+		node.next = this.head;
+		this.head = node;
 	}
 	pop() {
 		// removes the last node from the list and returns it
-        let tail = this.head
+		let tail = this.head;
 
-        while(tail.next.next !== null) {
-            tail = tail.next
-        }
-        
-        let last = tail.next
-        tail.next = null
-        return last
+		while (tail.next.next !== null) {
+			tail = tail.next;
+		}
+
+		let last = tail.next;
+		tail.next = null;
+		return last;
 	}
 	removeFromFront() {
 		// remove the head node from the list and return it
 		// the next node in the list is the new head node
-        let temp = this.head.next
-        let removed = this.head
-        removed.next = null
-        this.head = temp
-        return removed
+		let temp = this.head.next;
+		let removed = this.head;
+		removed.next = null;
+		this.head = temp;
+		return removed;
 	}
 	insertAt(X, data) {
 		// insert a new node into the list with the given data
 		// place it after X nodes in the list
 		// if X exceeds the bounds of the list, put the node at the end
 		// insertAt(0, 7) would add the new node as the head
-        let node = new Node(data)
+        const node = new Node(data);
+		
+        if (!this.head) {
+			this.head = new Node(data);
+			return;
+		}
 
-        let insertionPoint = this.head
-        while(X > 0 ) {
-            if (insertionPoint.next === null) {
-                insertionPoint.next = node
-                return node
-            }
-            insertionPoint = insertionPoint.next
-            X --
-        }
+		if (X === 0) {
+            node.next = this.head
+			this.head = node;
+			return node
+		}
 
-        
+		const previous = this.getAt(X - 1);
+		node.next = previous.next;
+		previous.next = node;
 
-
-        console.log(insertionPoint)
+		return node
 	}
 	removeAt(X) {
 		// remove the Xth node from the list, considering 0 to be the first node
@@ -90,14 +92,31 @@ class LinkedList {
 	sort() {
 		// sort the Linked List in ascending order of data values
 	}
+	getAt(index) {
+		let counter = 0;
+		let node = this.head;
+		while (node) {
+			if (counter === index) {
+				return node;
+			}
+			counter++;
+			node = node.next;
+		}
+		return null;
+	}
 }
 
-const list = new LinkedList()
+const list = new LinkedList();
 
-list.appendNode(4)
-list.appendNode(6)
-list.appendNode(1)
-list.prependNode(17)
-list.insertAt(2,1)
+list.appendNode(1);
+list.appendNode(2);
+list.appendNode(3);
+list.appendNode(4);
+list.appendNode(5);
+list.appendNode(6);
+list.appendNode(7);
+list.appendNode(8);
 
-console.log(list)
+list.getAt(0);
+
+console.log(list);
