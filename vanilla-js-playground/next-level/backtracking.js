e468cc109812c74a325c30322cf0210195c8da78
atlfinesse <finesse.cap@gmail.com>
2021-03-14 14:06:02 -0700

    Added backtracking to next-level folder

---
+++ vanilla-js-playground/next-level/backtracking.js
@@ -0,0 +1,14 @@
+// Backtracking is a method to solve problems by making a series of choices that we can return or backtrack to
+
+// the call stack "remembers" our choices and "knows" what to choose next
+
+// 3 KEYS
+    // 1. Our Choice 
+        // What choice do we make at each call of the function?
+        // recursion expresses decision
+    // 2. Our Constraints
+        // When do we stop following a certain path?
+        // When do we not waste time and attempt a certain option
+    // 3. Our goal
+        // What's our target?
+        // What are we trying to find?
