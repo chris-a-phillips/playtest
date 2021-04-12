bf09a3c5a4f33e4d80807365be42a8b3f908b9d2
Chris Phillips <phillipsachris@gmail.com>
2021-04-12 11:11:37 -0700

    Added extra sort function

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Wassup Planet
+    <div>
+      
     </div>
   );
-}
+};
 
 export default App;
