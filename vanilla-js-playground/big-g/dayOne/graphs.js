97c0c995391ff40350889d6965c542c9b4eb80ef
Chris Phillips <phillipsachris@gmail.com>
2021-06-14 18:29:23 -0700

    Set up folders for practice

---
+++ vanilla-js-playground/big-g/dayOne/graphs.js
@@ -0,0 +1,16 @@
+// edges list + vertices list
+
+const vertices = ['A', 'B', 'C', 'D', 'E']
+
+const edges = [
+    ['A', 'B']
+    ['A', 'D']
+    ['B', 'C']
+    ['C', 'D']
+    ['C', 'E']
+    ['D', 'E']
+]
+
+// findAdjacentNodes
+
+// isConnected
