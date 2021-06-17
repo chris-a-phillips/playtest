b441563fd73f68a8edc4feffb7ae4058530b0d74
Chris Phillips <phillipsachris@gmail.com>
2021-06-16 20:25:41 -0700

    Added refactor of anagram function

--- vanilla-js-playground/big-g/problem-solving-patterns/anagrams.js
+++ vanilla-js-playground/big-g/problem-solving-patterns/anagrams.js
@@ -35,4 +35,32 @@ const validAnagram = (strOne, strTwo) => {
     return true
 }
 
-console.log(validAnagram('snow', 'wont'))
+const validAnagram2 = (first, second) => {
+    if(first.length !== second.length) {
+        return false
+    }
+
+    const lookup = {}
+
+    for (let i = 0; i < first.length; i++) {
+        const letter = first[i]
+        // if letter exists, increment, otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1
+    }
+
+    for (let i = 0; i < second.length; i++) {
+        const letter = second[i]
+        // if we can't find a letter or letter is zero then return false
+        if (!lookup[letter]) {
+            return false
+        } else {
+            lookup[letter] -= 1
+        }
+        console.log(lookup)
+    }
+
+    return true
+}
+
+
+console.log(validAnagram2('hiiii', 'hiiii'));
