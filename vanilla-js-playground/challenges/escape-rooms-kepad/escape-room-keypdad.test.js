a40464a65274f7659515c1cb635c6c254872e458
Chris Phillips <phillipsachris@gmail.com>
2021-06-20 16:59:19 -0700

    Readded escape rooms keypad challenge

---
+++ vanilla-js-playground/challenges/escape-rooms-kepad/escape-room-keypdad.test.js
@@ -0,0 +1,16 @@
+const solution = require('./escape-room-keypads');
+
+// describe('sample test', () => {
+// 	// individual test
+// 	it('sample test 1', () => {
+// 		expect(solution().toBe());
+// 	});
+// });
+
+describe('sample test', () => {
+	it('CORRECT ANSWER', function () {
+		let wordlist = ['APPLE', 'PLEAS', 'PLEASE'];
+		let keypads = ['AELWXYZ', 'AELPXYZ', 'AELPSXY', 'SAELPRT', 'XAEBKSY'];
+		expect(solution(wordlist, keypads)).toEqual([0, 1, 3, 2, 0]);
+	});
+});
